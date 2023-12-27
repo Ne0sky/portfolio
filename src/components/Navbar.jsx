435b33fa7ed358339132fd7de934855d1e490038
Ne0sky <99585944+Ne0sky@users.noreply.github.com>
2023-12-27 19:13:01 +0530

    update project section, refactor code, update navbar working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,13 +34,13 @@ const NavBar = () => {
         translateX:'-50%',
         zIndex: 999,
       }}
-      className='fixed text-white bottom-15 left-1/2 transform -translate-x-1/2 bg-indigo-800 max-w-full flex justify-center items-center w-[95%] lg:w-[500px]  border border-indigo-600 p-4 backdrop-filter backdrop-blur-sm bg-opacity-90 rounded-xl'
+      className='fixed text-white bottom-15 left-1/2 transform -translate-x-1/2 bg-black max-w-full flex justify-center items-center w-[95%] lg:w-[500px]  border border-lime-500 p-4 backdrop-filter backdrop-blur-sm bg-opacity-100 rounded-xl'
     >
       {/* Your navigation content here */}
-      <ul className='flex font-tertiary  items-center font-semibold w-full justify-around'>
-        <li className='hover:font-bold cursor-pointer '><Link to='home' smooth={true} duration={500}>Home</Link></li>
-        <li className='hover:font-bold cursor-pointer'><Link to='about' smooth={true} duration={500}>About</Link></li>
-        <li className='hover:font-bold cursor-pointer'><Link to='projects' smooth={true} duration={500}>Projects</Link></li>
+      <ul className='flex font-tertiary  items-center font-medium w-full justify-around'>
+        <li className='hover:font-bold cursor-pointer '><Link to='home' smooth={true} duration={500}  spy={true} activeClass='active-link'>Home</Link></li>
+        <li className='hover:font-bold cursor-pointer'><Link to='about' smooth={true} duration={500} spy={true} activeClass='active-link'>About</Link></li>
+        <li className='hover:font-bold cursor-pointer'><Link to='projects' smooth={true} duration={500} spy={true} activeClass='active-link'>Projects</Link></li>
         <li className='hover:font-bold cursor-pointer'>Contact</li>
         {/* Add more navigation items */}
       </ul>
--- src/components/Sidemenu.jsx
+++ src/components/Sidemenu.jsx
@@ -6,10 +6,10 @@ const Sidemenu = () => {
     <div className='sidebar relative'>
        
         <ul className='text-2xl  font-tertiary uppercase flex flex-col gap-4 z-10 relative'>
-            <li ><Link className=' hover:text-lime-500 ease-in-out duration-300 hover:cursor-pointer' to='home' smooth={true} duration={500}>Home</Link></li>
-            <li><Link className=' hover:text-lime-500 ease-in-out duration-300 hover:cursor-pointer' to='about' smooth={true} duration={500}>About</Link></li>
-            <li><Link className=' hover:text-lime-500 ease-in-out duration-300 hover:cursor-pointer' to='projects' smooth={true} duration={500}>Projects</Link></li>
-            <li><Link className=' hover:text-lime-500 ease-in-out duration-300 hover:cursor-pointer' to='contact' smooth={true} duration={500}>Contact</Link></li>
+            <li ><Link className=' hover:text-lime-500 ease-in-out duration-300 hover:cursor-pointer' to='home' smooth={true} duration={500} spy={true} activeClass='active-link'>Home</Link></li>
+            <li><Link className=' hover:text-lime-500 ease-in-out duration-300 hover:cursor-pointer' to='about' smooth={true} duration={500} spy={true} activeClass='active-link'>About</Link></li>
+            <li><Link className=' hover:text-lime-500 ease-in-out duration-300 hover:cursor-pointer' to='projects' smooth={true} duration={500} spy={true} activeClass='active-link'>Projects</Link></li>
+            <li><Link className=' hover:text-lime-500 ease-in-out duration-300 hover:cursor-pointer' to='contact' smooth={true} duration={500} spy={true} activeClass='active-link'>Contact</Link></li>
           </ul>
     </div>
   )
